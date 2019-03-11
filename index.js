62da163af82532021ba46f1d0ed1e01d9e27da4e
Christopher Cabell <cccabell@icloud.com>
2019-03-11 23:39:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll(`.ranked-list`);
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  
+  var target = document.getElementById(`grand-node`);
+  var child = target.children[0];
+  while (child) {
+    target = child;
+    child = target.children[0];
+  } 
+  return target;
+}
