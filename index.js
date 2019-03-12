9ab819b833d3b98880fc6dbb4d634c05e56738fd
Christopher Cabell <cccabell@icloud.com>
2019-03-12 00:31:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll(`.ranked-list`);
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  var target = document.getElementById(`grand-node`);
+  while (target.children.length > 0) {
+    target = target.children[0];
+  }
+  return target;
+}
